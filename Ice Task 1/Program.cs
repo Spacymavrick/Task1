0a7aac411368efec863b62059db7bd640de8aef7
Spacymavrick <96030302+Spacymavrick@users.noreply.github.com>
2024-09-25 21:34:47 +0200

    Add project files.

---
+++ Ice Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ice_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt the user for the input of values
+            Console.WriteLine("Input the first number to multiply: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the second number to multiply: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the third number to multiply: ");
+            int c = int.Parse(Console.ReadLine());
+            
+            // Calculate total by multiplying the values input by user 
+            int total = a * b * c;
+
+            // Display the equation and total 
+            Console.WriteLine(a +" x "+ b +" x "+ c +" = "+ total);
+            Console.ReadLine();
+
+        }
+    }
+}
